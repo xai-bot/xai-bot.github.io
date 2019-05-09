3235114a99081ce4ab04cd23d72a8496060a1497
Michał Kuźba <kmichael00968@gmail.com>
2019-05-10 01:23:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ export default {
     app: {
         gateway: "https://titanic-explainer.gateway.dialogflow.cloud.ushakov.co", // <- enter your gateway URL here, the function is just a helper function for my cloud integration. You don't normally need it
         muted: false, // <- mute microphone at start
-        start_suggestions: [], // <- array of suggestions, displayed at the start screen
+        start_suggestions: ["Get Started"], // <- array of suggestions, displayed at the start screen
         fallback_lang: 'en' // <- fallback language code, if history mode or network is unavailable
     },
     i18n: {
