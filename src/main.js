b30bbb04493edfe7819f4ce7e8d69311bf68362e
Michał Kuźba <kmichael00968@gmail.com>
2020-03-28 00:02:51 +0100

    big update to new version

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
-import '@babel/polyfill' // <- babel Polyfills
-import 'whatwg-fetch' // <- Fetch API Polyfill
-
 import Vue from 'vue'
-import App from './Components/App/App.vue'
+import App from './Views/App.vue'
 
-import config from './../config'
-import worker from './registerServiceWorker' // <- register service worker, disable it, when running in development mode
+import config from './Config'
+import translations from './Translations/translations.json'
+import { register_service_worker } from './Utils'
 
+register_service_worker()
+
+Vue.config.productionTip = false
 Vue.prototype.config = config // <- set config to global scope
+Vue.prototype.translations = translations // <- set translations to global scope
 
-/* (global) This code is going to tell us, if history mode can be activated on the client, so the application can be consumed without localstorage */
+/* (global) This code is going to tell us, if history mode can be activated on the client, so the application can be consumed without sessionStorage */
 Vue.prototype.history = () => {
     try {
-        localStorage.getItem('check')
+        sessionStorage.getItem('check')
         return true
     }
 
@@ -23,14 +25,8 @@ Vue.prototype.history = () => {
 
 /* (global) Currently selected language or fallback language (en). Needs to be a function, since it's reactive. No need for vuex there */
 Vue.prototype.lang = () => {
-    if(Vue.prototype.history()) return localStorage.getItem('lang') || config.app.fallback_lang
-
-    else {
-        return config.app.fallback_lang
-    }
+    if (Vue.prototype.history()) return sessionStorage.getItem('lang') || config.fallback_lang
+    return config.fallback_lang
 }
 
-new Vue({
-    el: '#app',
-    render: h => h(App)
-}).$mount('#app')
+new Vue({render: h => h(App)}).$mount('#app')
